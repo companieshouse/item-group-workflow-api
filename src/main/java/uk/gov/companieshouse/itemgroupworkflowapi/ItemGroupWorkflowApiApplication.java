9ab5b4cdf046c26cdf947c734d2242e719665ede
caismith-ch <csmith2@companieshouse.gov.uk>
2023-02-06 13:14:11 +0000

    Create skeleton application for item-group-workflow-api

---
+++ src/main/java/uk/gov/companieshouse/itemgroupworkflowapi/ItemGroupWorkflowApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.itemgroupworkflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemGroupWorkflowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemGroupWorkflowApiApplication.class, args);
+    }
+
+}
