f525c177d1ad328fefe0f2a859f92407f6fcbff8
rmcmurray-ch <rmcmurray@companieshouse.gov.uk>
2023-06-01 16:42:08 +0100

    update to api model

--- src/main/java/uk/gov/companieshouse/item/group/workflow/api/model/ItemGroupJsonPayload.java
+++ src/main/java/uk/gov/companieshouse/item/group/workflow/api/model/ItemGroupJsonPayload.java
@@ -2,28 +2,72 @@ package uk.gov.companieshouse.item.group.workflow.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class ItemGroupJsonPayload {
-    //
-    // company_number
-    //
-    @JsonProperty("company_number")
-    private String companyNumber;
-    public final String getCompanyNumber() {
-        return companyNumber;
+
+    @JsonProperty("delivery_details")
+    private DeliveryDetails deliveryDetails;
+    public DeliveryDetails getDeliveryDetails() {return deliveryDetails;}
+
+    @JsonProperty("items")
+    private ArrayList items;
+    public ArrayList getItems() {return items;}
+
+    @JsonProperty("links")
+    private Links links;
+    public Links getLinks() {return links;}
+
+    @JsonProperty("order_number")
+    private String orderNumber;
+    public String getOrderNumber() {
+        return orderNumber;
     }
-    //
-    // company_name
-    //
-    @JsonProperty("company_name")
-    private String companyName;
-    public String getCompanyName() {
-        return companyName;
+
+    @JsonProperty("ordered_at")
+    private LocalDateTime orderedAt;
+    public LocalDateTime getOrderedAt() {
+        return orderedAt;
+    }
+
+    @JsonProperty("ordered_by")
+    private OrderedBy orderedBy;
+    public OrderedBy getOrderedBy() {
+        return orderedBy;
+    }
+
+    @JsonProperty("payment_reference")
+    private String paymentReference;
+    public String getPaymentReference() {
+        return paymentReference;
+    }
+
+    @JsonProperty("reference")
+    private String reference;
+    public String getReference() {
+        return reference;
+    }
+
+    @JsonProperty("total_order_cost")
+    private String totalOrderCost;
+    public String getTotalOrderCost() {
+        return totalOrderCost;
     }
 
     @Override
     public String toString() {
         return "ItemGroupJsonPayload {" +
-            "company_number='" + companyNumber + '\'' +
-            ", company_name='" + companyName + '}';
+                "delivery_details='" + deliveryDetails +
+                ", items='" + items +
+                ", links='" + links +
+                " order_number='" + orderNumber +
+                " ordered_at='" + orderedAt +
+                " ordered_by='" + orderedBy +
+                " payment_reference='" + paymentReference +
+                " reference='" + reference +
+                " total_order_cost='" + totalOrderCost +
+                '}';
     }
 }
