99b7053aa47c02ece0f7d0f18b286d9fd4b52102
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-06-05 09:18:13 +0100

    DCAC-78: Repackage code following merge (missed earlier).

--- src/test/java/uk/gov/companieshouse/itemgroupworkflowapi/ItemGroupWorkflowApiApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/itemgroupworkflowapi/ItemGroupWorkflowApiApplicationTests.java
@@ -1,8 +1,7 @@
-package uk.gov.companieshouse.item.group.workflow.api;
+package uk.gov.companieshouse.itemgroupworkflowapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import uk.gov.companieshouse.itemgroupworkflowapi.ItemGroupWorkflowApiApplication;
 import uk.gov.companieshouse.itemgroupworkflowapi.config.MongoConfig;
 
 @SpringBootTest(classes = {ItemGroupWorkflowApiApplication.class, MongoConfig.class})
