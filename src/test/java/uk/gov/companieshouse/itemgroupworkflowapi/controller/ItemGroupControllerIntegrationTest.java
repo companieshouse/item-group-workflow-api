0bd7047373ee104d35fd7f5bf8fb0c64dcc46c43
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-06-06 11:26:32 +0100

    DCAC-78: Get fair weather integration test working again.

--- src/test/java/uk/gov/companieshouse/itemgroupworkflowapi/controller/ItemGroupControllerIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/itemgroupworkflowapi/controller/ItemGroupControllerIntegrationTest.java
@@ -1,12 +1,20 @@
 package uk.gov.companieshouse.itemgroupworkflowapi.controller;
 
 
+import org.bson.Document;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.companieshouse.itemgroupworkflowapi.repository.ItemGroupsRepository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,8 +26,8 @@ import static uk.gov.companieshouse.itemgroupworkflowapi.util.PatchMediaType.APP
 @SpringBootTest
 class ItemGroupControllerIntegrationTest {
 
-    // TODO DCAC-78 A more convincing URI?
-    private static final String PATCH_ITEM_URI = "/item-groups/1/items/1";
+    private static final String ITEM_GROUP_ID = "IG-922016-860413";
+    private static final String PATCH_ITEM_URI = "/item-groups/" + ITEM_GROUP_ID + "/items/111-222-333";
 
     private static final String PATCH_ITEM_BODY = "{\n" +
             "    \"digital_document_location\": " +
@@ -43,9 +51,23 @@ class ItemGroupControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ItemGroupsRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @AfterEach
+    void tearDown() {
+        repository.findById(ITEM_GROUP_ID).ifPresent(repository::delete);
+    }
+
     @Test
     @DisplayName("patch item handles valid request successfully")
     void patchItemSuccessfully() throws Exception {
+
+        setUpItemGroup();
+
         mockMvc.perform(patch(PATCH_ITEM_URI)
                         .header(REQUEST_ID_HEADER_NAME, REQUEST_ID)
                         .contentType(APPLICATION_MERGE_PATCH)
@@ -74,6 +96,12 @@ class ItemGroupControllerIntegrationTest {
                         .content(PATCH_ITEM_BODY_WITH_BAD_STATUS))
                 .andExpect(status().isBadRequest())
                 .andDo(print());
+    }
+
+    private void setUpItemGroup() throws IOException {
+        final String itemGroup =
+                new String(Files.readAllBytes(Paths.get("src/test/resources/testdata/item_group.json")));
+        mongoTemplate.insert(Document.parse(itemGroup), "item_groups");
     }
 
 }
