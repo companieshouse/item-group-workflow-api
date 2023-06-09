793ef0fd32af02e080905d2b4d6fcae70c1fcde0
rmcmurray-ch <rmcmurray@companieshouse.gov.uk>
2023-06-09 12:12:29 +0100

    WIP Commit 2 - 201 Created test and 400 Unsuccessful test

--- src/test/java/uk/gov/companieshouse/itemgroupworkflowapi/controller/ItemGroupControllerTest.java
+++ src/test/java/uk/gov/companieshouse/itemgroupworkflowapi/controller/ItemGroupControllerTest.java
@@ -12,65 +12,105 @@ import static org.hamcrest.Matchers.is;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import uk.gov.companieshouse.itemgroupworkflowapi.model.ItemGroup;
-import uk.gov.companieshouse.itemgroupworkflowapi.model.ItemGroupData;
+import uk.gov.companieshouse.itemgroupworkflowapi.model.*;
+
+import uk.gov.companieshouse.itemgroupworkflowapi.repository.ItemGroupsRepository;
 import uk.gov.companieshouse.itemgroupworkflowapi.service.ItemGroupsService;
 import uk.gov.companieshouse.itemgroupworkflowapi.validation.ItemGroupsValidator;
 import uk.gov.companieshouse.itemgroupworkflowapi.util.TestConstants;
 import uk.gov.companieshouse.itemgroupworkflowapi.logging.LoggingUtils;
 import uk.gov.companieshouse.logging.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Unit tests the {@link ItemGroupController} class.
  */
 @ExtendWith(MockitoExtension.class)
 class ItemGroupControllerTest {
-
-    @InjectMocks
-    private ItemGroupController controller;
-
-    @Mock
-    private ItemGroupData itemGroupData;
-
-    @Mock
-    private ItemGroupsService itemGroupsService;
-
-    @Mock
-    private ItemGroupsValidator itemGroupsValidator;
-
+    
     @Mock
     private Logger logger;
 
     @Mock
-    private LoggingUtils loggingUtils;
+    private ItemGroupsRepository itemGroupsRepository;
 
     @Test
     @DisplayName("Create ItemGroup is successful")
-    void createItemGroupSuccessful() {
+    void createItemGroupSuccessfulwith201Created() {
 
-        ItemGroupData itemGroupData = new ItemGroupData();
-        ItemGroup itemGroup = new ItemGroup();
+        final LoggingUtils logger = new LoggingUtils(this.logger);
+        final ItemGroupsService service = new ItemGroupsService(logger, itemGroupsRepository);
+        final ItemGroupsValidator validator = new ItemGroupsValidator();
+        final ItemGroupController controllerUnderTest = new ItemGroupController(logger, service, validator);
 
-        when(itemGroupsService.createItemGroup(itemGroupData)).thenReturn(itemGroup);
+        final ItemGroupData itemGroupData = generateValidItemGroupData();
+        final ItemGroup itemGroup = generateItemGroup();
+        itemGroup.setData(itemGroupData);
 
-        ResponseEntity<Object> response =
-                controller.createItemGroup(TestConstants.TOKEN_REQUEST_ID_VALUE, itemGroupData);
-
+        final ResponseEntity<Object> response = controllerUnderTest.createItemGroup("123", itemGroupData);
         assertThat(response.getStatusCode(), is(HttpStatus.CREATED));
-        assertThat(response.getBody(), is(response));
+
     }
 
     @Test
-    @DisplayName("Create ItemGroup has validation errors")
-    void createItemGroupValidationErrors()  {
+    @DisplayName("Create ItemGroup is unsuccessful")
+    void createItemGroupUnsuccessfulwith400BadRequest()  {
 
         // TO-DO IN HERE
 
-        ResponseEntity<Object> response =
-                controller.createItemGroup(TestConstants.TOKEN_REQUEST_ID_VALUE, itemGroupData);
-
-        assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+//        ResponseEntity<Object> response =
+//                controller.createItemGroup(TestConstants.TOKEN_REQUEST_ID_VALUE, itemGroupData);
+//
+//        assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
     }
+
+    /**
+     * Item Group Data generation
+     */
+    private ItemGroupData generateValidItemGroupData() {
+        final ItemGroupData itemGroupData = new ItemGroupData();
+        itemGroupData.setOrderNumber("123");
+
+        DeliveryDetails deliveryDetails = new DeliveryDetails();
+        deliveryDetails.setCompanyName("Test Company Name");
+        itemGroupData.setDeliveryDetails(deliveryDetails);
+
+        ItemCosts itemCost = new ItemCosts();
+        itemCost.setProductType("certificate");
+
+        List<ItemCosts> itemCosts = new ArrayList<>();
+        itemCosts.add(itemCost);
+
+        Links links = new Links();
+        links.setOrder("123");
+        itemGroupData.setLinks(links);
+
+        Item item = new Item();
+        item.setCompanyNumber("123");
+        item.setCompanyName("Test Company Name");
+        item.setDescriptionIdentifier("description");
+        item.setKind("certificate");
+        item.setItemCosts(itemCosts);
+
+        List<Item> items = new ArrayList<>();
+        items.add(item);
+        itemGroupData.setItems(items);
+
+        return itemGroupData;
+    }
+
+    /**
+     * ItemGroup generation
+     */
+
+    private ItemGroup generateItemGroup() {
+        final ItemGroup itemGroup = new ItemGroup();
+        itemGroup.setId("123456");
+        return itemGroup;
+    }
+
 }
 
 
