5003e79448b539a36438aaf6c26420c49edb62ae
mcolwellch <mcolwell@companieshouse.gov.uk>
2023-06-14 09:18:07 +0100

    ItemGroupControllerTest 201, 400, 409 tests added.

--- src/test/java/uk/gov/companieshouse/itemgroupworkflowapi/controller/ItemGroupControllerTest.java
+++ src/test/java/uk/gov/companieshouse/itemgroupworkflowapi/controller/ItemGroupControllerTest.java
@@ -6,7 +6,6 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,6 +31,7 @@ import uk.gov.companieshouse.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,6 +40,10 @@ class ItemGroupControllerTest {
     private static final String VALID_DELIVERY_COMPANY_NAME = "Delivery Test Company";
     private static final String VALID_ITEM_COMPANY_NAME = "Item Test Company";
     private static final String VALID_COMPANY_NUMBER = "IG-12345-67890";
+    private static final String INVALID_DESC_ID = "XXX";
+    private static final String INVALID_ITEM_KIND = "XXX";
+    private static final String INVALID_ITEM_COST_PRODUCT_TYPE = "XXX";
+    private static final String EXAMPLE_ERROR_MESSAGE = "Example error message";
 
     @InjectMocks
     ItemGroupController controller;
@@ -62,12 +66,7 @@ class ItemGroupControllerTest {
     private ItemGroupsRepository itemGroupsRepository;
 
     @Test
-    @DisplayName("empty test")
-    void emptyTest() {
-    }
-
-    @Test
-    @DisplayName("create item group return 201 created")
+    @DisplayName("create item group succeeds return 201 CREATED")
     void createItemGroupReturnCreated201()  throws Exception {
         final String requestId = "12345";
         final ItemGroupData itemGroupData = fairWeatherItemGroupsDto();
@@ -76,17 +75,16 @@ class ItemGroupControllerTest {
 
         when(itemGroupsService.createItemGroup(itemGroupData)).thenReturn(itemGroup);
         when(loggingUtils.getLogger()).thenReturn(logger);
-
-        final ResponseEntity<Object> response = controller.createItemGroup(requestId, itemGroupData);
         //
         // Verify HttpStatus.CREATED returned.
         //
+        final ResponseEntity<Object> response = controller.createItemGroup(requestId, itemGroupData);
         assertThat(response.getStatusCode(), is(HttpStatus.CREATED));
         //
         // ItemGroup
         //
         final ItemGroup createdItemGroup = (ItemGroup) response.getBody();
-        assert(createdItemGroup != null);
+        assertNotNull(createdItemGroup);
         assertThat(createdItemGroup.getData().getOrderNumber(), is(VALID_ORDER_NUMBER));
 
         assertThat(createdItemGroup.getData().getOrderNumber(), is(VALID_ORDER_NUMBER));
@@ -107,96 +105,56 @@ class ItemGroupControllerTest {
         // ItemCosts
         //
         assertEquals(1, item.getItemCosts().size());
-        ItemCosts createdItemCosts = item.getItemCosts().get(0);
-        assertNotNull(createdItemCosts);
-
-        assertThat(createdItemCosts.getProductType(), is(ItemCostProductType.CERTIFIED_COPY_INCORPORATION.toString()));
+        ItemCosts itemCosts = item.getItemCosts().get(0);
+        assertNotNull(itemCosts);
+        assertThat(itemCosts.getProductType(), is(ItemCostProductType.CERTIFIED_COPY_INCORPORATION.toString()));
     }
 
     @Test
-    @DisplayName("create item group return 400 invalid request")
-    void createItemGroupReturnCreated400()  throws Exception {
+    @DisplayName("validation fails return 400 BAD_REQUEST")
+    void validationFailsReturnBadRequest400()  throws Exception {
+        final String requestId = "12345";
+        final ItemGroupData itemGroupData = invalidItemGroupsDtoMissingAllValidationCriteria();
+        ItemGroup itemGroup = new ItemGroup();
+        itemGroup.setData(itemGroupData);
+
+        when(requestValidator.validateCreateItemData(itemGroupData)).thenReturn(Arrays.asList(EXAMPLE_ERROR_MESSAGE));
+        when(loggingUtils.getLogger()).thenReturn(logger);
+
+        final ResponseEntity<Object> response = controller.createItemGroup(requestId, itemGroupData);
+
+        assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
+
+        List<String> errors = (List<String>) response.getBody();
+        assertNotNull(errors);
+        assertEquals(1, errors.size());
+        assertThat(errors.get(0), is(EXAMPLE_ERROR_MESSAGE));
+    }
+
+    @Test
+    @DisplayName("item group already exists return 409 CONFLICT")
+    void itemGroupAlreadyExistsReturnConflict409()  throws Exception {
         final String requestId = "12345";
         final ItemGroupData itemGroupData = fairWeatherItemGroupsDto();
         ItemGroup itemGroup = new ItemGroup();
         itemGroup.setData(itemGroupData);
+        List<String> xxx = Arrays.asList(EXAMPLE_ERROR_MESSAGE);
 
-        when(itemGroupsService.createItemGroup(itemGroupData)).thenReturn(itemGroup);
+        when(itemGroupsService.doesItemGroupExist(itemGroupData)).thenReturn(true);
         when(loggingUtils.getLogger()).thenReturn(logger);
 
         final ResponseEntity<Object> response = controller.createItemGroup(requestId, itemGroupData);
-        //
-        // Verify HttpStatus.BAD_REQUEST returned.
-        //
-        assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
-        //
-        // ItemGroup
-        //
-        final ItemGroup createdItemGroup = (ItemGroup) response.getBody();
-        assert(createdItemGroup != null);
-        assertThat(createdItemGroup.getData().getOrderNumber(), is(VALID_ORDER_NUMBER));
 
-        assertThat(createdItemGroup.getData().getOrderNumber(), is(VALID_ORDER_NUMBER));
-        assertThat(createdItemGroup.getData().getDeliveryDetails().getCompanyName(), is(VALID_DELIVERY_COMPANY_NAME));
-        assertThat(createdItemGroup.getData().getLinks().getOrder(), is(VALID_ORDER_NUMBER));
-        //
-        // Item
-        //
-        assertEquals(1, createdItemGroup.getData().getItems().size());
-        Item item = createdItemGroup.getData().getItems().get(0);
-        assertNotNull(item);
+        assertThat(response.getStatusCode(), is(HttpStatus.CONFLICT));
 
-        assertThat(item.getCompanyNumber(), is(VALID_COMPANY_NUMBER));
-        assertThat(item.getCompanyName(), is(VALID_ITEM_COMPANY_NAME));
-        assertThat(item.getDescriptionIdentifier(), is(ItemDescriptionIdentifier.CERTIFIED_COPY.toString()));
-        assertThat(item.getKind(), is(ItemKind.ITEM_CERTIFIED_COPY.toString()));
-        //
-        // ItemCosts
-        //
-        assertEquals(1, item.getItemCosts().size());
-        ItemCosts createdItemCosts = item.getItemCosts().get(0);
-        assertNotNull(createdItemCosts);
-
-        assertThat(createdItemCosts.getProductType(), is(ItemCostProductType.CERTIFIED_COPY_INCORPORATION.toString()));
+        ItemGroupData returnedItemGroupData = (ItemGroupData) response.getBody();
+        assertNotNull(returnedItemGroupData);
+        assertThat(returnedItemGroupData, is(itemGroupData));
     }
     /**
      * Fair weather with all validation triggered DTO
      */
     private ItemGroupData fairWeatherItemGroupsDto() {
-        final ItemGroupData dto = new ItemGroupData();
-//        dto.setOrderNumber(VALID_ORDER_NUMBER);
-//
-//        DeliveryDetails deliveryDetails = new DeliveryDetails();
-//        deliveryDetails.setCompanyName(VALID_DELIVERY_COMPANY_NAME);
-//        dto.setDeliveryDetails(deliveryDetails);
-//
-//        ItemCosts itemCost = new ItemCosts();
-//        itemCost.setProductType(ItemCostProductType.CERTIFIED_COPY_INCORPORATION.toString());
-//
-//        List<ItemCosts> itemCosts = new ArrayList<>();
-//        itemCosts.add(itemCost);
-//
-//        Links links = new Links();
-//        links.setOrder(VALID_ORDER_NUMBER);
-//        dto.setLinks(links);
-//
-//        Item item = new Item();
-//        item.setCompanyNumber(VALID_COMPANY_NUMBER);
-//        item.setCompanyName(VALID_ITEM_COMPANY_NAME);
-//        item.setDescriptionIdentifier(ItemDescriptionIdentifier.CERTIFIED_COPY.toString());
-//        item.setKind(ItemKind.ITEM_CERTIFIED_COPY.toString());
-//        item.setItemCosts(itemCosts);
-//
-//        List<Item> items = new ArrayList<>();
-//        items.add(item);
-//        dto.setItems(items);
-
-        return dto;
-    }
-    /**
-     * Invalidate DTO
-     */
-    private ItemGroupData invalidItemGroupsDto() {
         final ItemGroupData dto = new ItemGroupData();
         dto.setOrderNumber(VALID_ORDER_NUMBER);
 
@@ -224,6 +182,14 @@ class ItemGroupControllerTest {
         List<Item> items = new ArrayList<>();
         items.add(item);
         dto.setItems(items);
+
+        return dto;
+    }
+    /**
+     * Invalid DTO. No validation criteria met, at all...
+     */
+    private ItemGroupData invalidItemGroupsDtoMissingAllValidationCriteria() {
+        final ItemGroupData dto = new ItemGroupData();
 
         return dto;
     }
