38f3bd8e017b5e1622a060499efd69dffa7de7c0
tombradley1 <tbradley@companieshouse.gov.uk>
2023-06-27 14:35:56 +0100

    cleanup UserAuthenticationInterceptor

--- src/main/java/uk/gov/companieshouse/itemgroupworkflowapi/interceptor/UserAuthenticationInterceptor.java
+++ src/main/java/uk/gov/companieshouse/itemgroupworkflowapi/interceptor/UserAuthenticationInterceptor.java
@@ -23,29 +23,25 @@ public class UserAuthenticationInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         Map<String, Object> logMap = new HashMap<>();
         logMap.put(REQUEST_ID_LOG_KEY, request.getHeader(REQUEST_ID_HEADER_NAME));
-        //
-        // Any value will do for ERIC-Identity-Type so long as it is NOT empty.
-        //
+
         String identityType = EricHeaderHelper.getIdentityType(request);
         if(identityType == null) {
             logMap.put(IDENTITY_TYPE_LOG_KEY, MISSING_REQUIRED_INFO);
             logMap.put(STATUS_LOG_KEY, UNAUTHORIZED);
             LOGGER.infoRequest(request, "UserAuthenticationInterceptor error: no ERIC-Identity-Type", logMap);
             response.setStatus(UNAUTHORIZED.value());
-            return(false);  // NOT Authorised.
+            return false;  // NOT Authorised.
         }
-        //
-        // Any value will do for ERIC-Identity so long as it is NOT empty.
-        //
+
         String identity = EricHeaderHelper.getIdentity(request);
         if(identity == null) {
             logMap.put(IDENTITY_LOG_KEY, MISSING_REQUIRED_INFO);
             logMap.put(STATUS_LOG_KEY, UNAUTHORIZED);
             LOGGER.infoRequest(request, "UserAuthenticationInterceptor error: no ERIC-Identity", logMap);
             response.setStatus(UNAUTHORIZED.value());
-            return(false);  // NOT Authorised.
+            return false;  // NOT Authorised.
         }
 
-        return(true);   // Authorised OK...
+        return true;   // Authorised OK...
     }
 }
