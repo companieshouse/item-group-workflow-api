660c4a6a14b6ac2adac4ac787396764d8aba14cc
tombradley1 <tbradley@companieshouse.gov.uk>
2023-06-29 13:29:03 +0100

    remove unused logging constants

--- src/main/java/uk/gov/companieshouse/itemgroupworkflowapi/logging/LoggingUtilsConfiguration.java
+++ src/main/java/uk/gov/companieshouse/itemgroupworkflowapi/logging/LoggingUtilsConfiguration.java
@@ -8,12 +8,7 @@ import uk.gov.companieshouse.logging.LoggerFactory;
 @Configuration
 public class LoggingUtilsConfiguration {
     public static final String APPLICATION_NAMESPACE = "item-group-workflow-api";
-    public static final String REQUEST_ID_LOG_KEY = "request_id";
     public static final String REQUEST_ID_HEADER_NAME = "X-Request-ID";
-    public static final String STATUS_LOG_KEY = "status";
-    public static final String IDENTITY_LOG_KEY = "ERIC Identity";
-    public static final String IDENTITY_TYPE_LOG_KEY = "ERIC Identity Type";
-    public static final String MISSING_REQUIRED_INFO = "Required information missing";
 
     @Bean
     Logger getLogger(){
