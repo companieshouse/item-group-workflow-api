4beb2dd71b66c57afcc2260b0229233909019562
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-12-08 14:34:41 +0000

    DCAC-80: Use AvroSerializer to reduce producer boilerplate.

--- src/main/java/uk/gov/companieshouse/itemgroupworkflowapi/config/KafkaConfig.java
+++ src/main/java/uk/gov/companieshouse/itemgroupworkflowapi/config/KafkaConfig.java
@@ -1,5 +1,8 @@
 package uk.gov.companieshouse.itemgroupworkflowapi.config;
 
+import consumer.serialization.AvroSerializer;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,60 +12,43 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import uk.gov.companieshouse.itemgroupprocessed.ItemGroupProcessed;
-import uk.gov.companieshouse.itemgroupworkflowapi.kafka.ItemGroupProcessedAvroSerializer;
-import uk.gov.companieshouse.itemgroupworkflowapi.kafka.ItemOrderedCertifiedCopyAvroSerializer;
 import uk.gov.companieshouse.itemorderedcertifiedcopy.ItemOrderedCertifiedCopy;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
 
     @Bean
-    public ProducerFactory<String, ItemOrderedCertifiedCopy> producerFactory(
-            @Value("${spring.kafka.bootstrap-servers}" ) final String bootstrapServers) {
-        final Map<String, Object> config = new HashMap<>();
-        config.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ItemOrderedCertifiedCopyAvroSerializer.class);
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        return new DefaultKafkaProducerFactory<>(config);
+    public ProducerFactory<String, ItemOrderedCertifiedCopy> itemOrderedCertifiedCopyProducerFactory(
+        @Value("${spring.kafka.bootstrap-servers}") final String bootstrapServers) {
+        return new DefaultKafkaProducerFactory<>(createProducerConfig(bootstrapServers));
     }
 
     @Bean
-    public KafkaTemplate<String, ItemOrderedCertifiedCopy> kafkaTemplate(
-            @Value("${spring.kafka.bootstrap-servers}" ) final String bootstrapServers) {
-        return new KafkaTemplate<>(producerFactory(bootstrapServers));
-    }
-
-    @Bean
-    public ItemOrderedCertifiedCopyAvroSerializer avroSerializer() {
-        return new ItemOrderedCertifiedCopyAvroSerializer();
+    public KafkaTemplate<String, ItemOrderedCertifiedCopy> itemOrderedCertifiedCopyKafkaTemplate(
+        @Value("${spring.kafka.bootstrap-servers}") final String bootstrapServers) {
+        return new KafkaTemplate<>(itemOrderedCertifiedCopyProducerFactory(bootstrapServers));
     }
 
     @Bean
     public ProducerFactory<String, ItemGroupProcessed> itemGroupProcessedProducerFactory(
-        @Value("${spring.kafka.bootstrap-servers}" ) final String bootstrapServers) {
-        final Map<String, Object> config = new HashMap<>();
-        config.put(
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-            StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ItemGroupProcessedAvroSerializer.class);
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        return new DefaultKafkaProducerFactory<>(config);
+        @Value("${spring.kafka.bootstrap-servers}") final String bootstrapServers) {
+        return new DefaultKafkaProducerFactory<>(createProducerConfig(bootstrapServers));
     }
 
     @Bean
     public KafkaTemplate<String, ItemGroupProcessed> itemGroupProcessedKafkaTemplate(
-        @Value("${spring.kafka.bootstrap-servers}" ) final String bootstrapServers) {
+        @Value("${spring.kafka.bootstrap-servers}") final String bootstrapServers) {
         return new KafkaTemplate<>(itemGroupProcessedProducerFactory(bootstrapServers));
     }
 
-    @Bean
-    public ItemGroupProcessedAvroSerializer itemGroupProcessedAvroSerializer() {
-        return new ItemGroupProcessedAvroSerializer();
+    private static Map<String, Object> createProducerConfig(final String bootstrapServers) {
+        final Map<String, Object> config = new HashMap<>();
+        config.put(
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+            StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class);
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return config;
     }
 
 }
