32bbc6ec157a0f35acf557b221bd3edf2ec8a5da
foman-ch <foman@companieshouse.gov.uk>
2024-06-07 13:27:19 +0100

    configuring url pattern matching for trailing slash

--- src/main/java/uk/gov/companieshouse/itemgroupworkflowapi/config/ApplicationConfiguration.java
+++ src/main/java/uk/gov/companieshouse/itemgroupworkflowapi/config/ApplicationConfiguration.java
@@ -19,9 +19,11 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ApplicationConfiguration {
+public class ApplicationConfiguration implements WebMvcConfigurer {
 
     @Bean
     @Primary
@@ -58,6 +60,11 @@ public class ApplicationConfiguration {
         final List<HttpMessageConverter<?>> converters = new ArrayList<>();
         converters.add(new MappingJackson2HttpMessageConverter());
         return converters;
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
     }
 
 }
